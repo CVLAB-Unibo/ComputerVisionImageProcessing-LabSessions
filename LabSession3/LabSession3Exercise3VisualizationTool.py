12191dba4e53d95ccfdc4d435b43da7227f9fb23
Pierluigi Zama Ramirez <zamapierluigi@hotmail.it>
2019-10-24 21:14:00 +0200

    added python version visual tool

---
+++ LabSession3/LabSession3Exercise3VisualizationTool.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Visualization Tool
+# ## Computer Vision and Image Processing - Lab Session 3 Exercises
+# ### Prof: Luigi Di Stefano, luigi.distefano@unibo.it
+# ### Tutor: Pierluigi Zama Ramirez, pierluigi.zama@unibo.it
+
+# Use this tool to get the **coordinate** of pixels. Modify the variable *image_path* with the absolute path to an image. Run the code below and you will se a pop-up windows opening. Click on a pixel on the image and you will get the coordinate **(row,column)** of that pixel.
+
+# In[1]:
+
+
+# Define here your path
+image_path = "chessboards/0.jpg"
+
+
+# In[2]:
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+
+fig = plt.figure(figsize=(20,30))
+
+img=cv2.imread(image_path)
+
+def onclick(event):
+    ix, iy = event.xdata, event.ydata
+    print("Coordinate clicked pixel (row,column): [{},{}]".format(int(round(ix)), int(round(iy))))
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
+imgplot = plt.imshow(img)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
